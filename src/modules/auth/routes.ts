b011b31a398e323559cc653f5f6b2944cb2541fa
Renaanprofissional <renaan.profissional@gmail.com>
2025-10-07 22:11:45 -0300

    fix: ajustes

--- src/modules/auth/routes.ts
+++ src/modules/auth/routes.ts
@@ -1,10 +1,19 @@
 import { FastifyInstance } from "fastify";
 import { AuthController } from "./controller.js";
-
+import { verifyToken } from "@/lib/jwt.js";
 
 export async function authRoutes(app: FastifyInstance) {
   app.post("/register", AuthController.register);
   app.post("/login", AuthController.login);
   app.post("/logout", AuthController.logout);
-  
+  app.get("/me", async (req, reply) => {
+    try {
+      const token = req.cookies.token;
+      if (!token) return reply.status(401).send({ message: "Não autenticado" });
+      const decoded = verifyToken(token);
+      return reply.send({ user: decoded });
+    } catch (err) {
+      return reply.status(401).send({ message: "Token inválido" });
+    }
+  });
 }
